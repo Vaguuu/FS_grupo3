e8b57121b9250e98a2f3e0b357e13200c2f2b2c1
Vaguuu <medilagosto@gmail.com>
2023-05-25 17:44:58 -0300

    FINAL

--- js/script.js
+++ js/script.js
@@ -64,3 +64,22 @@ formulario.addEventListener('submit', (e) => {
         document.getElementById('error_formulario').classList.add('error_formulario-activo')
     }
 })
+
+fetch('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=gynecology&api_key=7ad003292d8815fd4519873844d413418407')
+  .then(response => response.json())
+  .then(data => {
+    // Filtra los resultados para mostrar solo las publicaciones de ginecología
+    const results = data.esearchresult.idlist;
+
+    // Muestra los resultados en tu página web
+    const resultsContainer = document.getElementById('results-container');
+    results.forEach(id => {
+      const resultElement = document.createElement('p');
+      resultElement.textContent = id;
+      resultsContainer.appendChild(resultElement);
+    });
+  })
+  .catch(error => {
+    // Manejo de errores
+    console.error(error);
+  });
